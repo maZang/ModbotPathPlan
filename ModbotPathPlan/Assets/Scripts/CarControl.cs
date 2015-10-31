969dacf43f70df320550ca64767472a5def7dab3
Matthew Zang <matthewzang1@dhcp-hol-611.eduroam.cornell.edu>
2015-10-31 13:12:09 -0400

    basic map and kart

---
+++ ModbotPathPlan/Assets/Scripts/CarControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControl : MonoBehaviour {
+
+	public Transform[] wheels;
+	private Rigidbody rb; 
+	public float speed; 
+
+	private float power;
+	private float steer;
+	public float maxSteer = 25.0f;
+	public float enginePower = 150.0f;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.centerOfMass = new Vector3(0.0f,0.3f,0.7f);
+	}
+
+	void Update() {
+		power = Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0f;
+		steer = Input.GetAxis("Horizontal") * maxSteer;
+
+		GetCollider(0).steerAngle = steer;
+		GetCollider(1).steerAngle = steer;
+		GetCollider(0).motorTorque = power;
+		GetCollider(1).motorTorque = power;
+		GetCollider(2).motorTorque = power;
+		GetCollider(3).motorTorque = power;
+	}
+
+	private WheelCollider GetCollider(int n) {
+		return wheels[n].gameObject.GetComponent<WheelCollider>();
+	}
+
+}
---
+++ ModbotPathPlan/Assets/Scripts/GenerateMesh.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateMesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NavMeshTriangulation navmesh = NavMesh.CalculateTriangulation();
+
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		mesh.vertices = navmesh.vertices;
+		mesh.triangles = navmesh.indices;
+
+		Vector2[] uvs = new Vector2[mesh.vertices.Length];
+
+		for (int i = 0; i < uvs.Length; i++) {
+			uvs[i] = new Vector2(mesh.vertices[i].x, mesh.vertices[i].z);
+		}
+		mesh.uv = uvs;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
