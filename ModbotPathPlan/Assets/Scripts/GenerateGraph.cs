ed11eb47ee23f55636aad142fb7ae2ed51edab78
Matthew Zang <matthewzang1@dhcp-mcfad-1469.eduroam.cornell.edu>
2015-11-01 12:59:35 -0500

    Some new files and scripts

---
+++ ModbotPathPlan/Assets/Scripts/GenerateGraph.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateGraph {
+
+	public Triangle[] nodes; 
+
+	public GenerateGraph() {
+
+		//get nav mesh characteristics from pre-made nav mesh. Will write script later that generates a nav-mesh for any map.
+		NavMeshTriangulation navmesh = NavMesh.CalculateTriangulation ();
+
+		//initialize triangles array
+		nodes = new Triangle[navmesh.indices.Length/3];
+
+		//Made sure nav mesh indices is a multiple of 3
+		for (int i = 0; i < navmesh.indices.Length / 3; i++) {
+			nodes[i] = new Triangle(navmesh.vertices[navmesh.indices[i*3]], navmesh.vertices[navmesh.indices[i*3 + 1]], 
+			               navmesh.vertices[navmesh.indices[i*3 + 2]], NavMesh.GetAreaCost(navmesh.areas[i]));
+		}
+	}
+
+	public override string ToString() {
+		string return_string = "";
+		foreach (Triangle triangle in nodes) {
+			 return_string += "\n" + (triangle.ToString ());
+		}
+		return return_string;
+	}
+}
---
+++ ModbotPathPlan/Assets/Scripts/PathPlanning.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathPlanning : MonoBehaviour {
+
+	public GenerateGraph map;
+	// Use this for initialization
+	void Start () {
+		map = new GenerateGraph ();
+		print (map.ToString ());
+	}
+
+	//draw nodes as spheres for debugging purposes
+	void OnDrawGizmosSelected() {
+		foreach (Triangle triangle in map.nodes) {
+			Gizmos.color = new Color(2f, 2f, 2f);
+			Gizmos.DrawSphere (triangle.Centroid(), 0.3f);
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine	(triangle.vertex1, triangle.vertex2);
+			Gizmos.DrawLine (triangle.vertex1, triangle.vertex3);
+			Gizmos.DrawLine (triangle.vertex2, triangle.vertex3);
+			Gizmos.DrawSphere (triangle.vertex1, 0.2f);
+			Gizmos.DrawSphere (triangle.vertex2, 0.2f);
+			Gizmos.DrawSphere (triangle.vertex3, 0.2f);
+		}
+	}
+
+}
