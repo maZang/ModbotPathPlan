6b3549963401ef15671f4845c5d60d7875be8d65
Matthew Zang <msz45@cornell.edu>
2015-11-14 21:57:38 -0500

    idk

---
+++ ModbotPathPlan/Assets/Scripts/ObstacleAvoid.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class ObstacleAvoid : MonoBehaviour {
+
+	public float visionRange;
+	public GameObject map; 
+	public bool avoidObstacle;
+
+	private List<RaycastHit> filterRaycastMap(List<RaycastHit> hits) {
+		List<GameObject> travelable = new List<GameObject>(GameObject.FindGameObjectsWithTag ("non-obstacle"));
+		List<RaycastHit> filteredhits = new List<RaycastHit>(); 
+		foreach (RaycastHit hit in hits) {
+			if (travelable.Contains(hit.collider.gameObject)) {
+				continue; 
+			}
+			filteredhits.Add(hit); 
+		}
+		return filteredhits;
+	}
+
+	private Collider findClosestObstacle() {
+		Collider closetObstacle = null; 
+		List<RaycastHit> hits = filterRaycastMap(new List<RaycastHit>(Physics.RaycastAll (transform.position, transform.forward, visionRange)));
+		float minimumDistance = visionRange;
+		foreach (RaycastHit hit in hits) {
+			if (hit.distance < minimumDistance) {
+				closetObstacle = hit.collider; 
+			}
+		}
+		return closetObstacle;
+	}
+
+	private void BingBangAvoidance(Collider obstacle) {
+
+	}
+
+	void FixedUpdate () {
+		Collider obstacle = findClosestObstacle ();
+		if (obstacle != null) {
+			avoidObstacle = true; 
+			BingBangAvoidance(obstacle);
+		}
+		print (obstacle); 
+	}
+}
