a644ad0589c136bfd7b5e0a862b96f5c8e454cd7
Kevin Jiang <kzj1995@yahoo.com>
2016-03-05 14:19:58 -0500

    Implement Kart portion of interface. Basically, it is a simplified version of CarControl that performs top level logic to path plan

--- ModbotPathPlan/Assets/Scripts/Kart.cs
+++ ModbotPathPlan/Assets/Scripts/Kart.cs
@@ -6,9 +6,20 @@ public class Kart : MonoBehaviour
 {
 	public List<Vector3> wayPoints; //list of waypoints for the car
 	public int current_point; //the current waypoint
+	PathPlanning pathPlanner; // Represents the path planning object
 
-	public void SetUpKart (List<Vector3> wayPoints, int current_point) {
-		this.wayPoints = wayPoints;
-		this.current_point = current_point;
+
+	public void SetUpKart () {
+		//Set initial waypoint to 0
+		current_point = 0;
+		//Perform path planning
+		GameObject pathPlanGameObject = GameObject.Find("GameMapWithNav");
+		pathPlanner = pathPlanGameObject.GetComponent<PathPlanning>();
+		pathPlanner.SetUpPathPlanning (transform.position);
+		wayPoints = new List<Vector3> ();
+		foreach (GenerateGraph.Node pathNode in pathPlanner.path) { 
+			wayPoints.Add(pathNode.point);
+		}
 	}
+
 }
